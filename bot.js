040c8f7a8b7d67fef882494cc7795712c67746c1
adamada246 <78761055+adamada246@users.noreply.github.com>
2021-10-07 19:43:33 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,30 +1,47 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const activator = new Discord.Client();
-global.status = '0';
+global.onoffind = '0';
 
 activator.on("message", (message) => {
-  if (global.status == '1'){
+  function main (message){
+  if (global.onoffind == '1'){
    if (message.content.startsWith("<@500076766996725760>") && message.channel.id == "819947069039968286"){
      message.channel.send("<@500076766996725760>")
    }
   }
    if (message.content.startsWith("start") && message.channel.id == "819947069039968286"){
-     global.status = '1';
+     global.onoffind = '1';
     message.channel.send("<@500076766996725760>")
   
   }
   if (message.content.startsWith("stop")){
       message.channel.send("k");
-     global.status = '0';
+     global.onoffind = '0';
   }
+}
+    try{
+        main(message)
+    }
+    catch{
+        main(message)
+    }
 });
 
 client.on('message', (messagea) => {
-  if (global.status == '1'){
+    function actvc (messagea){
+  if (global.onoffind == '1'){
   if (messagea.content.startsWith("<@500076766996725760>") && messagea.channel.id == "819947069039968286"){
     messagea.channel.send("<@500076766996725760>")
   }
+}
+try{
+    actvc(messagea)
+}
+catch{
+    actvc(messagea)
+}
+
 }
 
 });
