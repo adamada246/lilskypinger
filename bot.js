7f7031975296e184d9bb06962c65515777bb5db6
adamada246 <78761055+adamada246@users.noreply.github.com>
2021-10-09 20:02:14 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,27 +1,29 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const activator = new Discord.Client();
-global.onoffind = '0';
+const observer = new Discord.Client();
+global.mystatus = "0";
 
 activator.on("message", (message) => {
   function main (message){
-  if (global.onoffind == '1'){
+  if (mystatus == "1"){
    if (message.content.startsWith("<@500076766996725760>") && message.channel.id == "819947069039968286"){
      message.channel.send("<@500076766996725760>")
    }
   }
+}
+   
    if (message.content.startsWith("start") && message.channel.id == "819947069039968286"){
-     global.onoffind = '1';
+    global.mystatus = "1";
     message.channel.send("<@500076766996725760>")
+   
   
-  }
-  if (message.content.startsWith("stop")){
-      message.channel.send("k");
-     global.onoffind = '0';
-  }
+  
 }
     try{
+        
         main(message)
+        
     }
     catch{
         main(message)
@@ -30,7 +32,7 @@ activator.on("message", (message) => {
 
 client.on('message', (messagea) => {
     function actvc (messagea){
-  if (global.onoffind == '1'){
+  if (mystatus == "1"){
   if (messagea.content.startsWith("<@500076766996725760>") && messagea.channel.id == "819947069039968286"){
     messagea.channel.send("<@500076766996725760>")
   }
@@ -45,8 +47,24 @@ catch{
 
 
 
+
+
+
+
+
+});
+
+client.on('message', (messageb) => {
+
+    if (messageb.content.startsWith("stop")){
+        global.mystatus = "0";
+        messageb.reply("k");
+    }
+
+
 });
 
 //replace your bot token with the token you get from the discord dev website
 client.login(process.env.CLIENT_TOKEN);
 activator.login(process.env.ACTIVATOR_TOKEN);
+observer.login(process.env.OBSERVER_TOKEN);
